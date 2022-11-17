d6cdb294022b9b006edc8e56188667d68fb8fca3
Tekyto <Tekyto2014@gmail.com>
2022-11-17 19:36:55 +0200

    js_23_compound-interest-loop+

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function compoundInterest(startDeposit, annualRate, annualAmount, additionalInvestments, interestPeriod) {
-	// write your code here
+	const periodPercent = 1 + annualRate / annualAmount / 100;
+	const periodTerm = interestPeriod * annualAmount;
+	for ( let i = 0; i < periodTerm; i++) {
+		startDeposit *= periodPercent;
+		startDeposit += additionalInvestments;
+	}
+	return Math.round(startDeposit * 100) / 100;
 }
 
 module.exports = compoundInterest;
